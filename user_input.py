88e4c9eec8b69c071897d267bae60ccfadf9dde2
proWAA <124883094+proWAA@users.noreply.github.com>
2024-04-04 12:58:50 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+print("What is your name?")
+name = input()
+
+print("How old are you?")
+age = input()
+
+print("Where do you stay?")
+location = input()
+
+print('Hello {}, you are {} years old and you live in {}'.format(name, age, location))
